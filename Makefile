c600344e5c7e6cb32ce8cd6fead3afa432f66afd
Armin Sander <armin@replicator.org>
2021-10-16 21:04:05 +0200

    qn: Add a library that exposes the Tiff2Pdf functionality.

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: all
+all: build-lib
+
+.PHONY: publish-lib
+publish-lib: 
+	rm -f Tiff2PdfLib/bin/Release/*.nupkg
+	dotnet clean -c Release Tiff2PdfLib 
+	dotnet pack -c Release Tiff2PdfLib
+	dotnet nuget push Tiff2PdfLib/bin/Release/Tiff2PdfLib.*.nupkg --source ${QUESTNET_PACKAGES_NUGET_SOURCE} --api-key ${QUESTNET_PACKAGES_PUSH_API_KEY}
+
---
+++ Tiff2PdfLib/Lib.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using BitMiracle.LibTiff.Classic;
+using BitMiracle.Tiff2Pdf;
+
+namespace Tiff2PdfLib
+{
+    public static class Lib
+    {
+        const string UnnamedFile = "-";
+
+        /// Converts TIFF stream to a PDF stream.
+        public static void ConvertStream(Stream inputStream, Stream outputStream)
+        {
+            using var input = Tiff.ClientOpen(UnnamedFile, "r", inputStream, new TiffStream());
+            if (input == null)
+            {
+                throw new Exception($"Can't open input stream for reading");
+            }
+
+            var t2p = new T2P
+            {
+                m_testFriendly = false, 
+                m_outputfile = outputStream
+            };
+            t2p.validate();
+            
+            using var output = Tiff.ClientOpen(UnnamedFile, "w", t2p, t2p.m_stream);
+            if (output == null)
+            {
+                throw new Exception("Can't initialize output descriptor");
+            }
+
+            t2p.write_pdf(input, output);
+            if (t2p.m_error)
+            {
+                throw new Exception("An error occurred creating output PDF");
+            }
+        }
+    }
+}
